75f785762822ae08f688a5e7cf5bc8c4f660e07c
RahuBirla <birlarahul340@gmail.com>
2023-01-03 16:13:15 +0530

    Fetch and json data is upadted

--- New/Api.js
+++ New/Api.js
@@ -18,4 +18,41 @@
 // sessionStorage.setItem("name", "rahul");
 
 
+// fetch --> return --> promise --> then -- catch
+
+// Get method using fetch json
+// let data = fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((result) => { return result.json()})
+//     .then((value) => { console.log(value) })
+//     .catch((error)=>{console.log(error)});
+
+// Get method using fetch text
+let data = fetch("./dataApi.txt")
+    .then((result) => { return result.text()})
+    .then((value) => { console.log(value) })
+    .catch((error)=>{console.log(error)});
+
+// let data = fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({ name: "xyz", add: "indore" }),
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8'
+//     },
+// }).then((r) => { return r.json() })
+//     .then((v) => { console.log(v) })
+
+// console.log(data);
+
+
+// Delete
+
+let data1 = fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+})
+
+
+
+
+
+
 
---
+++ New/json.js
@@ -0,0 +1,38 @@
+// let obj = {
+//     name: "rahul",
+//     age: 23,
+//     add : "indore"
+// }
+
+
+// let obj = "{
+//     "name": "rahul",
+//     "age": 23,
+//     "add" : "indore"
+// }"
+
+
+// Stringify()
+
+// let obj = {
+//     name: "rahul",
+//     age: 23,
+//     add: "indore",
+//     demo:()=>{console.log("hello");}
+// }
+
+// console.log(obj);
+// let jsonData = JSON.stringify(obj)
+// console.log(jsonData);
+
+// Parse()
+// let jsonParse = JSON.parse(jsonData)
+// console.log(jsonParse);
+
+
+
+
+
+
+
+
