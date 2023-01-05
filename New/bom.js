f58eb27fc53d40578fb4f3cadb7639170ea8c68f
RahuBirla <birlarahul340@gmail.com>
2023-01-05 14:47:53 +0530

    BOM is updated

---
+++ New/bom.js
@@ -0,0 +1,47 @@
+// console.log(window.innerHeight);
+// console.log(window.outerHeight);
+// document.write(window.innerHeight)
+// document.write("------------------")
+// document.write(window.outerHeight)
+
+
+// console.log(window.innerWidth);
+// console.log(window.outerWidth);
+
+let x;
+const Webpage = () => {
+  x =  window.open("" , "_blank" , "width=200px , height=200px , left=200px , top=200px")
+}
+
+// const CloseWebpage = () => {
+//     x.close();
+// }
+
+
+// const moveto = () => {
+//     x.moveTo(100 , 100)
+// }
+
+// const moveby = () => {
+//     x.moveBy(200 , 200)
+// }
+
+
+// const resizeto = () => {
+//     x.resizeTo(216 , 280)
+// }
+
+// const resizeby = () => {
+//     x.resizeBy(200 , 200)
+// }
+
+
+// const scrollto = () => {
+//     window.scrollTo(200 , 0)
+// }
+
+// const scrollby = () => {
+//     window.scrollBy(-20 , -10)
+// }
+
+
